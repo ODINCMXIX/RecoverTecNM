412d2594efac835d7d568f4a09d16f363e1395a7
ODIN <oaps1999@gmail.com>
2024-09-23 11:15:18 -0600

    creacion de components

---
+++ src/app/components/crear-reporte/crear-reporte.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-reporte',
+  standalone: true,
+  imports: [],
+  templateUrl: './crear-reporte.component.html',
+  styleUrl: './crear-reporte.component.scss'
+})
+export class CrearReporteComponent {
+
+}
