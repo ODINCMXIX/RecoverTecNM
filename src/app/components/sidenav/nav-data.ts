65498aec8e90ae388088a222ea7e7f44e45bfada
ODIN <oaps1999@gmail.com>
2024-10-02 11:49:58 -0600

    navData, html and ts

---
+++ src/app/components/sidenav/nav-data.ts
@@ -0,0 +1,10 @@
+import { RouterLink } from "@angular/router";
+import { routes } from "../../app.routes";
+
+export const navbarData= [
+  {
+    RouterLink: 'dashboard',
+    icon: 'fal fa-home',
+    label: 'Dashboard'
+  }
+];
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import {navbarData} from './nav-data';
 
 @Component({
   selector: 'app-sidenav',
-  standalone: true,
-  imports: [],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
 export class SidenavComponent {
-
+  
+  collapsed = false;
+  navData = navbarData;
 }
