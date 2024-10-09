4d6197b062a6a39c5fcf0aeb982bd86aa31ae9c9
ODIN <oaps1999@gmail.com>
2024-10-09 12:53:07 -0600

    Conexion de carrerar al boton

--- src/app/components/inicio2/inicio2.component.ts
+++ src/app/components/inicio2/inicio2.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit} from '@angular/core';
-import { CommonModule } from '@angular/common'; // Importa CommonModule
+import { CommonModule } from '@angular/common'; 
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio2',
@@ -9,9 +10,18 @@ import { CommonModule } from '@angular/common'; // Importa CommonModule
   styleUrl: './inicio2.component.scss'
 })
 export class Inicio2Component {
+
+  constructor( private router: Router){}
+
   cards = [
-    { title: 'Reporte Docentes', image: 'assets/img/Reporte1.png' },
-    { title: 'Estadísticas', image: 'assets/img/Reporte2.png' },
-    { title: 'Reporte Trimestral', image: 'assets/img/Avisos1.png' }
+    { title: 'Reporte Docentes', image: 'assets/img/Reporte1.png', route:'carreras' },
+    { title: 'Estadísticas', image: 'assets/img/Reporte2.png', route: null },
+    { title: 'Reporte Trimestral', image: 'assets/img/Avisos1.png', route: null }
   ];
+
+  navigate(route: string | null): void{
+    if (route !==null){
+      this.router.navigate([`/${route}`]);
+    }
+  }
 }
